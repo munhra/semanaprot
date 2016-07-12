a3aa7ea977db2e7b5b50454a1874aaa8933897d7
vntlab <vntlab@suportes-Mac-mini-23.local>
2016-07-12 16:13:25 -0300

    Nova Classe

---
+++ SemanaPuc/Enemy.swift
@@ -0,0 +1,18 @@
+//
+//  Enemy.swift
+//  SemanaPuc
+//
+//  Created by vntlab on 7/12/16.
+//  Copyright © 2016 munhra. All rights reserved.
+//
+
+import Foundation
+
+
+class Enemy {
+    
+    var HP:Int?
+    var Nome:String?
+    
+    
+}
