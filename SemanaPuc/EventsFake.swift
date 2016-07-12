83e0bdaeacca04bda1bdc76289fa58c3b3cf31dc
munhra <rafael.munhoz@gmail.com>
2016-07-12 13:56:52 -0300

    Event model draft implemented

---
+++ SemanaPuc/EventsFake.swift
@@ -0,0 +1,14 @@
+//
+//  EventsFake.swift
+//  SemanaPuc
+//
+//  Created by Rafael M. A. da Silva on 7/12/16.
+//  Copyright © 2016 munhra. All rights reserved.
+//
+
+import Foundation
+
+class EventsFake
+{
+    
+}
